2a71d7ac2d8f81c4b5dff0228a4747bada97c3c9
Talha Ekhlas <ekhlas@du.ac.bd>
2022-03-08 16:06:56 +0600

    08032022 real post content

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class() ?>>
 <div class="header">
     <div class="container">
         <div class="row">
@@ -20,120 +20,70 @@
     </div>
 </div>
 <div class="posts">
-    <div class="post">
+    <?php while(have_posts()) {
+
+        the_post();
+        
+        ?>
+    <div class="post" <?php post_class(); ?>>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="post-title">This is a beautiful day in Dhaka!</h2>
+                    <h2 class="post-title">
+                        <a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
+                    </h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4">
                     <p>
-                        <strong>John Doe</strong><br/>
-                        15th May, 2018
-                    </p>
-                    <ul class="list-unstyled">
-                        <li>dhaka</li>
-                    </ul>
-                </div>
-                <div class="col-md-8">
-                    <p>
-                        <img class="img-fluid" src="https://images.pexels.com/photos/301929/pexels-photo-301929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=800"
-                             alt="Post Title">
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut deserunt dicta doloribus error,
-                        harum,
-                        illo impedit incidunt ipsam ipsum necessitatibus nihil perferendis perspiciatis provident quasi
-                        reiciendis saepe
-                        sequi sint, voluptatum?
-                    </p>
-
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium adipisci aspernatur
-                        beatae
-                        consequatur dicta distinctio ea eaque enim eos est et eveniet exercitationem expedita hic, id,
-                        minima
-                        molestias necessitatibus optio perferendis quaerat quidem reiciendis rem reprehenderit
-                        repudiandae
-                        saepe
-                        sunt totam ullam unde velit. A ab animi aperiam at beatae cum cupiditate dignissimos distinctio
-                        ducimus
-                        eaque est exercitationem illo labore laudantium magni maxime molestias odio quibusdam quidem,
-                        sequi
-                        soluta sunt ullam voluptate voluptates voluptatum? Dolores earum enim esse est, illo nemo sit
-                        velit.
-                        Aperiam, aspernatur cum explicabo illum iusto labore nam nobis quibusdam ratione sed suscipit
-                        unde
-                        voluptate voluptatibus. Alias distinctio est et laborum quis tempore! Autem consequuntur
-                        cupiditate
-
-                    </p>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="post">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="post-title">This is a beautiful day in Dhaka!</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <p>
-                        <strong>John Doe</strong><br/>
-                        15th May, 2018
+                        <strong><?php the_author(); ?></strong><br/>
+                        <?php echo get_the_date(); ?>
                     </p>
                     <div class="tags">
-                        <ul class="list-unstyled">
+                        <?php echo get_the_tag_list("<ul class=\"list-unstyled\"><li>", "</li><li>", "</li></ul>") ?>
+                        <!-- <ul class="list-unstyled">
                             <li>weather</li>
                             <li>blog</li>
                             <li>dhaka</li>
-                        </ul>
+                        </ul> -->
                     </div>
                 </div>
                 <div class="col-md-8">
                     <p>
-                        <img class="img-fluid" src="https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                             alt="Post Title">
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut deserunt dicta doloribus error,
-                        harum,
-                        illo impedit incidunt ipsam ipsum necessitatibus nihil perferendis perspiciatis provident quasi
-                        reiciendis saepe
-                        sequi sint, voluptatum?
+                        <?php
+
+                        if(has_post_thumbnail()){
+                            the_post_thumbnail('large', ["class"=>"image_fluid"]);
+                        }
+                        
+                        ?>
                     </p>
 
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium adipisci aspernatur
-                        beatae
-                        consequatur dicta distinctio ea eaque enim eos est et eveniet exercitationem expedita hic, id,
-                        minima
-                        molestias necessitatibus optio perferendis quaerat quidem reiciendis rem reprehenderit
-                        repudiandae
-                        saepe
-                        sunt totam ullam unde velit. A ab animi aperiam at beatae cum cupiditate dignissimos distinctio
-                        ducimus
-                        eaque est exercitationem illo labore laudantium magni maxime molestias odio quibusdam quidem,
-                        sequi
-                        soluta sunt ullam voluptate voluptates voluptatum? Dolores earum enim esse est, illo nemo sit
-                        velit.
-                        Aperiam, aspernatur cum explicabo illum iusto labore nam nobis quibusdam ratione sed suscipit
-                        unde
-                        voluptate voluptatibus. Alias distinctio est et laborum quis tempore! Autem consequuntur
-                        cupiditate
-
+                        <?php
+                         is_single()?the_content():the_excerpt();
+                        ?>
                     </p>
+                    
                 </div>
             </div>
 
         </div>
     </div>
+    <?php } ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8">
+                <?php the_posts_pagination(array('screen_reader_text'=>' ','mid_size'=>1)); ?>
+                pagination not working
+                <!-- why not working -->
+            </div>
+        </div>
+    </div>
 </div>
+
 <div class="footer">
     <div class="container">
         <div class="row">
